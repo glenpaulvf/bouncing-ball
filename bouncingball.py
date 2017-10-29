afa69bf1e4d5ed59870a1da456af2e16c2adc95f
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-28 21:16:32 -0700

    Display red circle at top-left corner

--- bouncingball.py
+++ bouncingball.py
@@ -1,12 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter
 from PyQt5.QtCore import Qt
+
 
 class BouncyBallWindow(QWidget):
     
     def __init__(self):
         super(BouncyBallWindow, self).__init__()
         self.init_UI()
+        
+        # Ball properties
+        self.diameter = 30
+        self.x = 0 # x-coordinate
+        self.y = 0 # y-coordinate
     
     def init_UI(self):
         # Set window dimensions to 600px by 400px
@@ -17,7 +24,17 @@ class BouncyBallWindow(QWidget):
         pal.setColor(self.backgroundRole(), Qt.white)
         self.setAutoFillBackground(True)
         self.setPalette(pal)
-        
+                     
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        self.draw(painter)
+    
+    def draw(self, painter):
+        painter.setPen(Qt.red)
+        painter.drawEllipse(self.x, self.y, self.diameter, self.diameter)
+        painter.end()
+
 
 def main():
     app = QApplication(sys.argv)
